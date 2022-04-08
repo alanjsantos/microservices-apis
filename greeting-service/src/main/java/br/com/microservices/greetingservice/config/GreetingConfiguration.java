bc0630b1c7738b33a8fa83b0f1ca1f3f40f966ab
Alan J Santos <alanjosedossantos011@gmail.com>
2022-04-07 21:41:52 -0300

    atualziando as configuracoes sem interromper os microservicos

--- greeting-service/src/main/java/br/com/microservices/greetingservice/config/GreetingConfiguration.java
+++ greeting-service/src/main/java/br/com/microservices/greetingservice/config/GreetingConfiguration.java
@@ -4,9 +4,11 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.stereotype.Component;
 
 @Component
+@RefreshScope
 @ConfigurationProperties("greeting-service")
 @Data
 @AllArgsConstructor
